3d6ccd16486096ef3248bd9c1195c48dc1982e83
Niels van Velzen <git@ndat.nl>
2018-03-28 22:45:29 +0200

    Reactive arrays + for loops (not 100% complete)

--- ReactiveClass.js
+++ ReactiveClass.js
@@ -2,37 +2,36 @@ const ReactiveSymbols = {
 	get: Symbol(),
 	set: Symbol(),
 	deleteProperty: Symbol(),
-	ownKeys: Symbol(),
-	has: Symbol(),
-	defineProperty: Symbol(),
-	getOwnPropertyDescriptor: Symbol(),
+	createProxy: Symbol()
 };
 
 export class ReactiveClass {
 	constructor() {
 		this._observers = [];
 
-		return new Proxy(this, {
-			get: this[ReactiveSymbols.get],
-			set: this[ReactiveSymbols.set],
-			deleteProperty: this[ReactiveSymbols.deleteProperty],
-			ownKeys: this[ReactiveSymbols.ownKeys],
-			has: this[ReactiveSymbols.has],
-			defineProperty: this[ReactiveSymbols.defineProperty],
-			getOwnPropertyDescriptor: this[ReactiveSymbols.getOwnPropertyDescriptor],
+		return this[ReactiveSymbols.createProxy](this);
+	}
+
+	[ReactiveSymbols.createProxy](obj) {
+		return new Proxy(obj, {
+			get: (...args) => this[ReactiveSymbols.get](...args),
+			set: (...args) => this[ReactiveSymbols.set](...args),
+			deleteProperty: (...args) => this[ReactiveSymbols.deleteProperty](...args)
 		});
 	}
 
 	[ReactiveSymbols.get](obj, prop) {
-		return prop === 'this' ? obj : obj[prop];
+		return obj[prop];
 	}
 
 	[ReactiveSymbols.set](obj, prop, val) {
 		if (val === obj[prop]) return true;
 
+		if (Array.isArray(val)) val = this[ReactiveSymbols.createProxy](val);
+
 		obj[prop] = val;
 
-		if (!prop.startsWith('_')) for (let observer of obj._observers) observer(prop);
+		if (!prop.startsWith('_')) for (let observer of this._observers) observer(prop);
 
 		return true;
 	}
--- ReactiveTemplate.js
+++ ReactiveTemplate.js
@@ -6,14 +6,11 @@ export class ReactiveTemplate extends ReactiveClass {
 
 		this._el = el;
 		this._tpl = el.innerHTML;
-		this._changes = [];
 		this._renderRequested = false;
 		this._templateMap = new Map();
+		this._forChilds = new Map();
 
-		this.observe(prop => {
-			this._changes.push(prop);
-			this.requestRender()
-		});
+		this.observe(prop => this.requestRender());
 		this.requestRender()
 	}
 
@@ -27,72 +24,100 @@ export class ReactiveTemplate extends ReactiveClass {
 	render() {
 		if (this._el.dataset.compiled !== 'true') {
 			this._el.innerHTML = this._tpl;
-			this._el.dataset.compiled = true;
+			this._el.dataset.compiled = 'true';
 			this._el.normalize();
 		}
 
-		this.renderElement(this._el);
+		this.renderElement(this._el, this);
 
-		this._changes = [];
 		this._renderRequested = false;
 	}
 
-	renderElement(node) {
+	renderElement(node, context) {
+		if (node.dataset && node.dataset.skip === 'true') return;
+
 		if (node.nodeType === 3) {
-			this.renderText(node);
+			this.renderText(node, context);
 		} else if (node.nodeType === 1) {
 			for (let i = node.attributes.length - 1; i >= 0; i--)
-				this.renderAttribute(node.attributes[i], node);
+				this.renderAttribute(node.attributes[i], node, context);
 		}
 
-		for (let child of node.childNodes) this.renderElement(child);
+		if (node.dataset && node.dataset.skip === 'true') return;
+		if (node.dataset && node.dataset.skipChilds === 'true') return;
+
+		for (let child of node.childNodes) this.renderElement(child, context);
 	}
 
-	renderText(node) {
+	renderText(node, context) {
 		if (node.nodeValue.trim().length > 0 && node.parentNode.childNodes.length > 1) {
 			let span = document.createElement('span');
 			span.textContent = node.nodeValue;
 			span.style.display = 'contents';
 			node.parentNode.replaceChild(span, node);
-			this.renderElement(span);
+			this.renderElement(span, context);
 		} else {
 			if (!this._templateMap.has(node)) this._templateMap.set(node, node.nodeValue);
 			
-			let text = this._templateMap.get(node).replace(/{{([\w\W]*)}}/g, (match, prop) => this.execFunction(this, prop));
+			let text = this._templateMap.get(node).replace(/{{([\w\W]+?)}}/g, (match, prop) => this.execFunction(this, prop, context));
 			if (node.nodeValue !== text) node.nodeValue = text;
 		}
 	}
 
-	renderAttribute(attr, node) {
+	renderAttribute(attr, node, context) {
 		if (attr.name.startsWith('@')) {
-			node.addEventListener(attr.name.substr(1), () => this.execFunction(node, attr.value));
+			node.addEventListener(attr.name.substr(1), () => this.execFunction(node, attr.value, context));
 
 			node.attributes.removeNamedItem(attr.name);
 		} else if (attr.name.startsWith(':')) {
 			let name = attr.name.substr(1);
-			let value = this.execFunction(node, attr.value);
+			let value = this.execFunction(node, attr.value, context);
 
 			if(['value', 'checked'].includes(name)) node[name] = value;
 			else node.setAttribute(name, value);
+		} else if (attr.name === 'v-for') {
+			if (!this._templateMap.has(node)) this._templateMap.set(node, node.cloneNode(true));
+			let tpl = this._templateMap.get(node);
+
+			let [, left, operator, expression] = attr.value.match(/(.+?)\s*(in|of)\s*(.+)/);
+			expression = this.execFunction(node, expression, context);
+
+			if (this._forChilds.has(node))
+				for (let child of this._forChilds.get(node)) child.parentNode.removeChild(child);
+			
+			this._forChilds.set(node, []);
+
+			for (let item of expression) {
+				let child = tpl.cloneNode(true);
+				child.attributes.removeNamedItem(attr.name);
+				node.parentNode.insertBefore(child, node);
+
+				this.renderElement(child, {...context, [left]: item});
+
+				this._forChilds.get(node).push(child);
+			}
+
+			node.dataset.skipChilds = 'true';
 		}
 	}
 
-	execFunction(self, value) {
+	execFunction(self, value, context) {
 		try {
 			let code = [];
 			let tmpVarName = 'templateDataCollection';
-			code.push(`let {${Object.keys(this).join(',')}} = ${tmpVarName};`);
+			code.push(`let {${Object.keys(context).join(',')}} = ${tmpVarName};`);
 			code.push('/*-*/');
-			code.push('const execFunctionResult = ' + new Function('return ' + value).toString() + '.call(this);');
+			code.push(`const execFunctionResult = ${new Function('return ' + value).toString()}.call(this);`);
 			code.push('/*-*/');
-			code.push(`return {execFunctionResult, vars: {${Object.keys(this).join(',')}}}`);
+			code.push(`return {execFunctionResult, vars: {${Object.keys(context).join(',')}}}`);
 
 			let func = new Function(tmpVarName, code.join('\r\n'));
-			let result = func.call(self, this);
+			let result = func.call(self, context);
 		
-			for (let entry of Object.entries(result.vars)) this[entry[0]] = entry[1];
+			for (let entry of Object.entries(result.vars)) context[entry[0]] = entry[1];
 			return result.execFunctionResult;
 		} catch (err) {
+			console.log({ self, value, context });
 			console.error(err);
 			return;
 		}
